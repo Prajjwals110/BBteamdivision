d1a570b19238576dc7c8e87ea534239e581287ff
Prajjwal <prajjwals107@gmail.com>
2022-09-20 14:27:21 +0530

    Added my project

---
+++ script.js
@@ -0,0 +1,372 @@
+let products = {
+    data: [
+      {
+        productName: "Aakash",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/1.jfif",
+      },
+      {
+        productName: "Ankit",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/2.jpg",
+      },
+      {
+        productName: "Nihal",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/3.jpg",
+      },      
+      {
+        productName: "Prajjwal",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/4.jpg",
+      },
+      {
+        productName: "Rakesh",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/5.jpg",
+      },
+      {
+        productName: "Abhishek",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Rest 12",
+        image: "players/6.jpg",
+      },
+      {
+        productName: "Biplav",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/7.jpg",
+      },
+      {
+        productName: "Bhavuk",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/8.jpg",
+      },
+      {
+        productName: "Aarish",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/9.jpg",
+      },      
+      {
+        productName: "Chandan",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/10.jpg",
+      },
+      {
+        productName: "Shailesh",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/11.jpg",
+      },
+      {
+        productName: "Adarsh",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Rest 12",
+        image: "players/12.jpg",
+      }, 
+      {
+        productName: "Tarun",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/13.jpg",
+      },
+      {
+        productName: "Aman",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/14.jpg",
+      },
+      {
+        productName: "Pawan",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/15.jpg",
+      },      
+      {
+        productName: "Vishal",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/16.jpg",
+      },
+      {
+        productName: "Raghav",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/17.jpg",
+      },
+      {
+        productName: "Lucky",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Rest 12",
+        image: "players/18.jpg",
+      },       
+      {
+        productName: "Rocky",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/19.jpg",
+      },
+      {
+        productName: "Vicky",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/20.jpg",
+      },
+      {
+        productName: "Shri",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/21.jpg",
+      },      
+      {
+        productName: "Shivay",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/22.jpg",
+      },
+      {
+        productName: "Hasim",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/23.jpg",
+      },
+      {
+        productName: "Aarif",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Alpha",
+        image: "players/24.jpg",
+      }, 
+      {
+        productName: "Araham",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/25.jpg",
+      },
+      {
+        productName: "Mukesh",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/26.jpg",
+      },
+      {
+        productName: "Tony",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/27.jpg",
+      },      
+      {
+        productName: "Steve",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/28.jpg",
+      },
+      {
+        productName: "Bruce",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/29.jpg",
+      },
+      {
+        productName: "Joe",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Rest 12",
+        image: "players/30.jpg",
+      }, 
+      {
+        productName: "Jos",
+        category: "Alpha",
+        Age: "22",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/31.jpg",
+      },
+      {
+        productName: "Clark",
+        category: "Beta",
+        Age: "23",
+        POS: "PF",
+        team: "Beta",
+        image: "players/32.jpg",
+      },
+      {
+        productName: "Hugh",
+        category: "Rest",
+        Age: "24",
+        POS: "SG",
+        team: "Rest 12",
+        image: "players/33.jpeg",
+      },      
+      {
+        productName: "Kobe",
+        category: "Alpha",
+        Age: "25",
+        POS: "SF",
+        team: "Alpha",
+        image: "players/34.jpg",
+      },
+      {
+        productName: "Micahel",
+        category: "Beta",
+        Age: "26",
+        POS: "PF",
+        team: "Beta",
+        image: "players/35.jpg",
+      },
+      {
+        productName: "Harden",
+        category: "Rest",
+        Age: "27",
+        POS: "C",
+        team: "Rest 12",
+        image: "players/36.jpeg",
+      }, 
+    ],
+  };
+  
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //Age
+    let Age = document.createElement("h6");
+    Age.innerText = "AGE : " + i.Age;
+    container.appendChild(Age);
+    //Position name
+    let team = document.createElement("h5");
+    team.classList.add("POS");
+    team.innerText = "POS : " + i.POS.toUpperCase();
+    container.appendChild(team);
+    //Position
+    let POS = document.createElement("h5");
+    POS.classList.add("product-team");
+    POS.innerText = "TEAM : " + i.team.toUpperCase();
+    container.appendChild(POS);      
+    
+    
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
